049c604619bf90bac738f72d290719b2c3e15643
ludek <bednarludek@seznam.cz>
2022-03-21 10:34:07 +0100

    Verze 1.0

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def all_anagrams(words):
     if len(words) == 0:
         return False
     for i in words:
-        usporadana_slova.append(sorted(i))
+        if type(i) != str:
+            return False
+        else:
+            usporadana_slova.append(sorted(i))
+    for i in range(len(usporadana_slova)):
+        if usporadana_slova[i] in usporadana_slova[i:]:
+            return True
 
-
-
+print(all_anagrams(['ship', 'hips', 'name']))
 
